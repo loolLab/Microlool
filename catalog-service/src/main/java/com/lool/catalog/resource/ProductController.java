ec550a04c801247b4700c9d8bf0d7548225cea24
patrikejane <sskmal@gmail.com>
2018-10-17 15:02:41 +0530

    @RefreshScope

--- catalog-service/src/main/java/com/lool/catalog/resource/ProductController.java
+++ catalog-service/src/main/java/com/lool/catalog/resource/ProductController.java
@@ -4,6 +4,8 @@ import com.lool.catalog.RuntimeException.ProductNotFoundException;
 import com.lool.catalog.service.ProductService;
 import com.lool.catalog.domain.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@RefreshScope
 @RequestMapping("/api/products")
 public class ProductController {
 
@@ -26,6 +29,15 @@ public class ProductController {
     public Product productByCode(@PathVariable String code) {
         return productService.findProductByCode(code)
                 .orElseThrow(() -> new ProductNotFoundException("Product with code ["+code+"] doesn't exist"));
+    }
+
+    @Value("${message: default}")
+    String name;
+
+    @GetMapping("/name")
+    public String name()
+    {
+        return name;
     }
 
 
