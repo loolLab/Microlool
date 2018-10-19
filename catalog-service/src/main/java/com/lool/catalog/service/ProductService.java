e03c8243e5e742bed6883a1877ef1bdf19fd6980
patrikejane <sskmal@gmail.com>
2018-10-19 07:36:47 +0530

    Hystrix DashBoard

--- catalog-service/src/main/java/com/lool/catalog/service/ProductService.java
+++ catalog-service/src/main/java/com/lool/catalog/service/ProductService.java
@@ -3,6 +3,9 @@ package com.lool.catalog.service;
 import com.lool.catalog.domain.Product;
 import com.lool.catalog.repository.ProductRepository;
 import com.lool.catalog.util.ProductInventoryDto;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import com.netflix.ribbon.proxy.annotation.Hystrix;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,7 +29,20 @@ public class ProductService {
     public List<Product> findAllProducts() {
         return (List<Product>) productRepository.findAll();
     }
+
+    @HystrixCommand(fallbackMethod = "getProductInventoryByCode", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000"),
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50")
+        }
+    )
     public Optional<ProductInventoryDto> findProductByCode(String code) {
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         Optional<Product> productOp = productRepository.findByCode(code);
         if(productOp.isPresent()){
             ResponseEntity<ProductInventoryDto> prodResponceEntity = restTemplate.getForEntity("http://inventory-service/api/inventory/{code}",ProductInventoryDto.class,code);
@@ -40,6 +56,13 @@ public class ProductService {
             }
         }
         return Optional.empty();
+    }
+
+    Optional<ProductInventoryDto> getProductInventoryByCode(String code){
+        ProductInventoryDto prodResponceEntity = new ProductInventoryDto();
+        prodResponceEntity.setProductCode(code);
+        prodResponceEntity.setAvailableQuantity(0);
+        return Optional.ofNullable(prodResponceEntity);
     }
 
 }
